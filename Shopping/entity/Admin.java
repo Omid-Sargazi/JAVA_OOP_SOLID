5987d2d29df1376c5d4635c89abfc7603a55d4df
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-05 02:09:33 +0330

    Admin (Subclass of User)

---
+++ Shopping/entity/Admin.java
@@ -0,0 +1,14 @@
+package Shopping.entity;
+
+public class Admin extends User {
+
+    public Admin(String username, String password) {
+        super(username, password);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Admin [Username=" + getUsername() + "]");
+
+    }
+}
