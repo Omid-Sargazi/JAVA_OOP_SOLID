c9798ad54b16ec26fc4fe49591f817c53c1e183f
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-06 19:10:28 +0330

    Controller Layer UserController

---
+++ Library/controller/UserController.java
@@ -0,0 +1,47 @@
+package Library.controller;
+
+import Library.entity.User;
+import java.util.Map;
+import java.util.HashMap;
+import Library.factory.UserFactory;
+
+public class UserController {
+    private Map<String, User> users;
+    private User loggedInUser;
+
+    public UserController() {
+        users = new HashMap<>();
+        loggedInUser = null;
+    }
+
+    public void registerUser(String type, String username, String password) {
+        User user = UserFactory.createUser(type, username, password);
+        users.put(username, user);
+    }
+
+    public boolean loginUser(String username, String password) {
+        User user = users.get(username);
+        if (user != null && user.getPassword().equals(password)) {
+            loggedInUser = user;
+            return true;
+        }
+        return false;
+    }
+
+    public void logoutUser() {
+        loggedInUser = null;
+    }
+
+    public boolean changePassword(String oldPassword, String newPassword) {
+        if (loggedInUser != null && loggedInUser.getPassword().equals(oldPassword)) {
+            loggedInUser.setPassword(newPassword);
+            return true;
+        }
+        return false;
+    }
+
+    public User getLoggedInUser() {
+        return loggedInUser;
+    }
+
+}
