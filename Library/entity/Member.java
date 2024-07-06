cddae70a8d83ea5f517a1bdd4ce2109c6ffe5764
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-06 18:41:23 +0330

    Member (Subclass of User)

---
+++ Library/entity/Member.java
@@ -0,0 +1,13 @@
+package Library.entity;
+
+public class Member extends User {
+
+    public Member(String username, String password) {
+        super(username, password);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Member [Username=" + getUsername() + "]");
+    }
+}
