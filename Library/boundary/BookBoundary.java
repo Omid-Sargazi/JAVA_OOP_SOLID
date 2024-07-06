a910dfff09ab68200daeb99325e25a8f51bc16da
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-07 00:11:44 +0330

    BookBoundary

---
+++ Library/boundary/BookBoundary.java
@@ -0,0 +1,52 @@
+package Library.boundary;
+
+import Library.controller.BookController;
+import Library.entity.Book;
+import java.util.List;
+
+public class BookBoundary {
+    private BookController bookController;
+
+    public BookBoundary(BookController bookController) {
+        this.bookController = bookController;
+    }
+
+    public void addBook(String id, String title, String author) {
+        Book book = new Book(id, title, author);
+        bookController.addBook(book);
+        System.out.println("Book added: " + book);
+    }
+
+    public void removeBook(String bookId) {
+        bookController.removeBook(bookId);
+        System.out.println("Book removed with ID: " + bookId);
+    }
+
+    public void searchBook(String keyword) {
+        List<Book> results = bookController.searchBook(keyword);
+        System.out.println("Search results:");
+        for (Book book : results) {
+            System.out.println(book);
+        }
+    }
+
+    public void borrowBook(String bookId) {
+        Book book = bookController.getBookById(bookId);
+        if (book != null) {
+            bookController.borrowBook(book);
+            System.out.println("Book borrowed: " + book);
+        } else {
+            System.out.println("Book not found with ID: " + bookId);
+        }
+    }
+
+    public void returnBook(String bookId) {
+        Book book = bookController.getBookById(bookId);
+        if (book != null) {
+            bookController.returnBook(book);
+            System.out.println("Book returned: " + book);
+        } else {
+            System.out.println("Book not found with ID: " + bookId);
+        }
+    }
+}
