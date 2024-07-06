2888505201f837309c7b7ca79fc5c0df1eac6ac3
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-07 00:18:22 +0330

    Entity Layer User (Base Class)

---
+++ Projects/Library/User.java
@@ -0,0 +1,25 @@
+package Projects.Library;
+
+public abstract class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public abstract void showDetails();
+}
