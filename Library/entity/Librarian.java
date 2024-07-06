bdb44de924a4c9788ed0f134e7e21f7b92f967aa
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-06 18:43:10 +0330

    Librarian (Subclass of User)

---
+++ Library/entity/Librarian.java
@@ -0,0 +1,12 @@
+package Library.entity;
+
+public class Librarian extends User {
+    public Librarian(String username, String password) {
+        super(username, password);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Librarian [Username=" + getUsername() + "]");
+    }
+}
