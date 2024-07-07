1a92e5dbf41f724d8b42d9e2da693947ffde40fd
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-08 01:32:46 +0330

    UserController Class in controller layer

---
+++ Projects/LibrarySimplified/controller/UserController.java
@@ -0,0 +1,47 @@
+package Projects.LibrarySimplified.controller;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import Projects.LibrarySimplified.entity.User;
+
+public class UserController {
+    private List<User> users;
+    private User loggedInUser;
+
+    public UserController() {
+        this.users = new ArrayList<>();
+        this.loggedInUser = null;
+    }
+
+    public void registerUser(String type, String username, String password) {
+        User user = new User(username, password, type);
+        users.add(user);
+    }
+
+    public boolean loginUser(String username, String password) {
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                loggedInUser = user;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void logoutUser() {
+        loggedInUser = null;
+    }
+
+    public boolean changePassword(String oldPassword, String newPassword) {
+        if (loggedInUser != null && loggedInUser.getPassword().equals(oldPassword)) {
+            loggedInUser.setPassword(newPassword);
+            return true;
+        }
+        return false;
+    }
+
+    public User getLoggedInUser() {
+        return loggedInUser;
+    }
+}
