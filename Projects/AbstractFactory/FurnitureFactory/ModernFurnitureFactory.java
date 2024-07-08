8f036717bdcbc0e19935b377ee8309b0e6d0613e
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-08 18:55:21 +0330

    Concrete Factories

---
+++ Projects/AbstractFactory/FurnitureFactory/ModernFurnitureFactory.java
@@ -0,0 +1,15 @@
+package Projects.AbstractFactory.FurnitureFactory;
+
+public class ModernFurnitureFactory implements FurnitureFactory {
+    @Override
+    public Chair createChair() {
+        // TODO Auto-generated method stub
+        return new ModernChair();
+    }
+
+    @Override
+    public Sofa createSofa() {
+        // TODO Auto-generated method stub
+        return new ModernSofa();
+    }
+}
---
+++ Projects/AbstractFactory/FurnitureFactory/VictorianFurnitureFactory.java
@@ -0,0 +1,15 @@
+package Projects.AbstractFactory.FurnitureFactory;
+
+public class VictorianFurnitureFactory implements FurnitureFactory {
+    @Override
+    public Chair createChair() {
+        // TODO Auto-generated method stub
+        return new VictorianChair();
+    }
+
+    @Override
+    public Sofa createSofa() {
+        // TODO Auto-generated method stub
+        return new VictorianSofa();
+    }
+}
