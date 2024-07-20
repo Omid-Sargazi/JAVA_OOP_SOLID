7b005129140d61b2dd7d6c9bd66403407b7bf217
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-20 22:42:40 +0330

    Windows Factory

---
+++ Projects/AbstractFactoryExample/WindowsFactory.java
@@ -0,0 +1,11 @@
+package Projects.AbstractFactoryExample;
+
+public class WindowsFactory implements GUIFactory {
+    public Button createButton() {
+        return new WindowsButton();
+    }
+
+    public Checkbox createCheckbox() {
+        return new WindowsCheckbox();
+    }
+}
