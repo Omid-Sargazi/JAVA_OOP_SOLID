eb60599b6f1cfb3dab3f261a3328a67171e23c18
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-22 00:20:03 +0330

    Concrete Product A2

---
+++ Projects/AbstractFactory/AbstractFactoryExample02/WildDog.java
@@ -0,0 +1,7 @@
+package Projects.AbstractFactory.AbstractFactoryExample02;
+
+public class WildDog implements Animal {
+    public void move() {
+        System.out.println("Wild dog is moving.");
+    }
+}
