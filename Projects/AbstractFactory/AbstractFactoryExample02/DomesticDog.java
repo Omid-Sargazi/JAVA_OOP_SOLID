e8da0ead4bc8425ca6a28d216044debee246fde2
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-22 00:17:40 +0330

    Concrete Product A1

---
+++ Projects/AbstractFactory/AbstractFactoryExample02/DomesticDog.java
@@ -0,0 +1,7 @@
+package Projects.AbstractFactory.AbstractFactoryExample02;
+
+public class DomesticDog implements Animal {
+    public void move() {
+        System.out.println("Domestic dog is moving.");
+    }
+}
