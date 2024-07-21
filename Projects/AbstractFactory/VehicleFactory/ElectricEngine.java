7dbefd22b130aad7c3184b93446489481b0b1943
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-22 00:27:05 +0330

    Concrete Product B1

---
+++ Projects/AbstractFactory/VehicleFactory/ElectricEngine.java
@@ -0,0 +1,7 @@
+package Projects.AbstractFactory.VehicleFactory;
+
+public class ElectricEngine implements Engine {
+    public void start() {
+        System.out.println("Starting an electric engine.");
+    }
+}
