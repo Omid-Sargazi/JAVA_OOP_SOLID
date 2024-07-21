4e28b5892167065fe90308a652c9baad586f7ee4
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-22 00:20:50 +0330

    Concrete Product B2

---
+++ Projects/AbstractFactory/AbstractFactoryExample02/WildDogSound.java
@@ -0,0 +1,7 @@
+package Projects.AbstractFactory.AbstractFactoryExample02;
+
+public class WildDogSound implements Sound {
+    public void makeSound() {
+        System.out.println("Wild dog is howling.");
+    }
+}
