db0836082a8a78c6a586d82ccdb45c2c6965bc0d
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-23 01:03:24 +0330

    Inheritance and Polymorphism

---
+++ Projects/LibraryManagementSystem/Librarian.java
@@ -0,0 +1,16 @@
+package Projects.LibraryManagementSystem;
+
+public class Librarian extends User {
+    public Librarian(String name, String id) {
+        super(name, id);
+    }
+
+    @Override
+    public void borrowBook(Book book) {
+        System.out.println(getName() + " issued " + book.getTitle());
+    }
+
+    public void addBook(Book book) {
+        System.out.println(getName() + " added " + book.getTitle());
+    }
+}
