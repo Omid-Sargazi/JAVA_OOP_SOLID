3eaa1e94dc1b12bddfc65c2201bc6afc10786d9a
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-23 00:56:01 +0330

    Concrete Product B2

---
+++ Projects/AbstractFactory/VehicleFactory/GasolineEngine.java
@@ -0,0 +1,7 @@
+package Projects.AbstractFactory.VehicleFactory;
+
+public class GasolineEngine implements Engine {
+    public void start() {
+        System.out.println("Starting a gasoline engine.");
+    }
+}
