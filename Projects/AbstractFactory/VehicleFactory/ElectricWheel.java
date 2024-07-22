0a1d76b4ea921bd462eed2a665dd954ce3df1929
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-23 00:54:26 +0330

    Concrete Product C1

---
+++ Projects/AbstractFactory/VehicleFactory/ElectricWheel.java
@@ -0,0 +1,7 @@
+package Projects.AbstractFactory.VehicleFactory;
+
+public class ElectricWheel implements Wheel {
+    public void rotate() {
+        System.out.println("Rotating electric wheels.");
+    }
+}
