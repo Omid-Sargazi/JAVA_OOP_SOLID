a8acf61e8eeaf3315902f9abd0b0f8c0ba842556
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-24 00:14:27 +0330

    Liskov Substitution Principle (LSP)

---
+++ Projects/LibraryManagementSystem/Library.java
@@ -0,0 +1,13 @@
+package Projects.LibraryManagementSystem;
+
+public class Library {
+    public static void main(String[] args) {
+        User member = new Member("Alice", "M001");
+        User librarian = new Librarian("Bob", "L001");
+
+        Book book = new Book("Effective Java", "Joshua Bloch", "978-0134685991");
+
+        member.borrowBook(book);
+        librarian.borrowBook(book);
+    }
+}
