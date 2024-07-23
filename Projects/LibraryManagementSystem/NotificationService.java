443183ebee0396dae775480a077a48ad38005f37
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-24 00:27:42 +0330

    Dependency Inversion Principle (DIP)

---
+++ Projects/LibraryManagementSystem/NotificationService.java
@@ -0,0 +1,13 @@
+package Projects.LibraryManagementSystem;
+
+public class NotificationService {
+    private Notification notification;
+
+    public NotificationService(Notification notification) {
+        this.notification = notification;
+    }
+
+    public void sendNotification(String message) {
+        notification.send(message);
+    }
+}
