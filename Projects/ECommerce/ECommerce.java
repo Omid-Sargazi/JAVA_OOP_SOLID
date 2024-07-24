15d6adbe78e90126f82fb2d62bd304f426eec7cf
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-25 00:39:05 +0330

    Liskov Substitution Principle (LSP)

---
+++ Projects/ECommerce/ECommerce.java
@@ -0,0 +1,13 @@
+package Projects.ECommerce;
+
+public class ECommerce {
+    public static void main(String[] args) {
+        User customer = new Customer("john_doe", "john@example.com");
+        User admin = new Admin("admin_user", "admin@example.com");
+
+        Product product = new Product("P001", "Laptop", 1200.00);
+
+        customer.purchase(product);
+        admin.purchase(product);
+    }
+}
