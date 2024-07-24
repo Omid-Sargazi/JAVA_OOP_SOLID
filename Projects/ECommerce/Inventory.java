0dadf44b17eaedc83cb73e6ac72efaa4e085980e
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-25 00:36:22 +0330

    Single Responsibility Principle (SRP)

---
+++ Projects/ECommerce/Inventory.java
@@ -0,0 +1,20 @@
+package Projects.ECommerce;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory {
+    private List<Product> products;
+
+    public Inventory() {
+        products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
