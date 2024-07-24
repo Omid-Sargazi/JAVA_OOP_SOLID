ad128abc78dd41cc9c7b9983a871e223352b0b0a
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-25 00:35:42 +0330

    Inheritance and Polymorphism

---
+++ Projects/ECommerce/Customer.java
@@ -0,0 +1,14 @@
+package Projects.ECommerce;
+
+public class Customer extends User {
+
+}
+
+    public Customer(String username, String email) {
+        super(username, email);
+    }
+
+@Override
+public void purchase(Product product) {
+    System.out.println(getUsername() + " purchased " + product.getName());
+}
