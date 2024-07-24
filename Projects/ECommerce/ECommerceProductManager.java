1f4d7a5046fb2df38d02a7d801be83735be2cb29
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-25 00:45:06 +0330

    Interface Segregation Principle (ISP)

---
+++ Projects/ECommerce/ECommerceProductManager.java
@@ -0,0 +1,13 @@
+package Projects.ECommerce;
+
+public class ECommerceProductManager implements ProductManager {
+    @Override
+    public void addProduct(Product product) {
+        System.out.println("Product added: " + product.getName());
+    }
+
+    @Override
+    public void removeProduct(Product product) {
+        System.out.println("Product removed: " + product.getName());
+    }
+}
