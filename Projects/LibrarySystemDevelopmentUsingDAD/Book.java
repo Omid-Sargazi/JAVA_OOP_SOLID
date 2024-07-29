0609a4a833e281ba783e1cdb1829851018496e80
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-30 01:06:26 +0330

    Create a Book class.

---
+++ Projects/LibrarySystemDevelopmentUsingDAD/Book.java
@@ -0,0 +1,13 @@
+package Projects.LibrarySystemDevelopmentUsingDAD;
+
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
