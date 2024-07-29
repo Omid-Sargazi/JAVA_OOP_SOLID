72b52e9ae6bee484774d3591371437cebb9e8663
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-30 01:12:35 +0330

    Extend the Library class to add and list members.

--- Projects/LibrarySystemDevelopmentUsingDAD/Library.java
+++ Projects/LibrarySystemDevelopmentUsingDAD/Library.java
@@ -5,13 +5,23 @@ import java.util.List;
 
 public class Library {
     private List<Book> books;
+    private List<Member> members;
 
     public Library() {
         this.books = new ArrayList<>();
+        this.members = new ArrayList<>();
     }
 
     public void addBook(Book book) {
         books.add(book);
+    }
+
+    public void addMember(Member member) {
+        members.add(member);
+    }
+
+    public List<Member> listMembers() {
+        return members;
     }
 
     public List<Book> listBooks() {
@@ -24,4 +34,9 @@ public class Library {
                     "Title: " + book.getTitle() + ", Author: " + book.getAuthor() + ", ISBN: " + book.getIsbn());
         }
     }
+
+    public void displayMembers() {
+        for (Member member : members) {
+            System.out.println("Name: " + member.getName() + ", Member ID: " + member.getMemberId());
+        }
 }
