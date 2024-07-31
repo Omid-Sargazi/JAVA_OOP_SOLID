0d17f0a923e9edcdc086eec618827575808cc20d
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-31 18:14:52 +0330

    Concrete Product A1

---
+++ Projects/AbstractFactory/VehicleFactory02/ElectricCar.java
@@ -0,0 +1,7 @@
+package Projects.AbstractFactory.VehicleFactory02;
+
+public class ElectricCar implements Vehicle {
+    public void drive() {
+        System.out.println("Driving an electric car.");
+    }
+}
