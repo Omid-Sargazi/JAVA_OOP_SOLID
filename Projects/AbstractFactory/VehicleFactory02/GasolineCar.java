5a9ab58c637c207d2e7c13906d2be1fcfea72f7c
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-31 18:17:55 +0330

    Concrete Product A2

---
+++ Projects/AbstractFactory/VehicleFactory02/GasolineCar.java
@@ -0,0 +1,7 @@
+package Projects.AbstractFactory.VehicleFactory02;
+
+public class GasolineCar implements Vehicle {
+    public void drive() {
+        System.out.println("Driving a gasoline car.");
+    }
+}
