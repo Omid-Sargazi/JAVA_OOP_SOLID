b73a361e3eb9a944d2fd0be1f5a3ca33a9ae4545
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-03 00:16:58 +0330

    Boundary Layer UserBoundary

---
+++ Projects/LibrarySystemDesignwithSOLIDPrinciples/boundary/UserBoundary.java
@@ -0,0 +1,47 @@
+package Projects.LibrarySystemDesignwithSOLIDPrinciples.boundary;
+
+import Projects.LibrarySystemDesignwithSOLIDPrinciples.controller.UserController;
+import Projects.LibrarySystemDesignwithSOLIDPrinciples.entity.User;
+
+public class UserBoundary {
+    private UserController userController;
+
+    public UserBoundary(UserController userController) {
+        this.userController = userController;
+    }
+
+    public void registerUser(String type, String username, String password) {
+        userController.registerUser(type, username, password);
+        System.out.println(type + " registered successfully.");
+    }
+
+    public void login(String username, String password) {
+        if (userController.loginUser(username, password)) {
+            System.out.println(username + " logged in successfully.");
+        } else {
+            System.out.println("Login failed.");
+        }
+    }
+
+    public void logout() {
+        userController.logoutUser();
+        System.out.println("User logged out successfully.");
+    }
+
+    public void changePassword(String oldPassword, String newPassword) {
+        if (userController.changePassword(oldPassword, newPassword)) {
+            System.out.println("Password changed successfully.");
+        } else {
+            System.out.println("Password change failed.");
+        }
+    }
+
+    public void showLoggedInUserDetails() {
+        User user = userController.getLoggedInUser();
+        if (user != null) {
+            user.showDetails();
+        } else {
+            System.out.println("No user is logged in.");
+        }
+    }
+}
