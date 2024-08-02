7ab1a771429fd5f8195066051dc6aefd833015be
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-02 22:10:40 +0330

    User (Base Class)

---
+++ Projects/LibrarySystemDesignwithSOLIDPrinciples/entity/User.java
@@ -0,0 +1,25 @@
+package Projects.LibrarySystemDesignwithSOLIDPrinciples.entity;
+
+public abstract class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public abstract void showDetails();
+}
