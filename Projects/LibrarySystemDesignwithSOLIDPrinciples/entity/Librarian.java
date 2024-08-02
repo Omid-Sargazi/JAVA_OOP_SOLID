6b566002c15ebe9341abc5f2a73392d7ecbec3e1
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-02 22:12:00 +0330

    Librarian (Subclass of User)

---
+++ Projects/LibrarySystemDesignwithSOLIDPrinciples/entity/Librarian.java
@@ -0,0 +1,13 @@
+package Projects.LibrarySystemDesignwithSOLIDPrinciples.entity;
+
+public class Librarian extends User {
+
+    public Librarian(String username, String password) {
+        super(username, password);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Librarian [Username=" + getUsername() + "]");
+    }
+}
