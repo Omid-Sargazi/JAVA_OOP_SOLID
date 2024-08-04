ab628a412fd5e54fdf137e3bb780ded847290ab6
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-05 00:43:30 +0330

    User Controller

---
+++ Projects/onlineshoppingsystem/controller/UserController.java
@@ -0,0 +1,45 @@
+package Projects.onlineshoppingsystem.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import Projects.onlineshoppingsystem.entity.User;
+
+public class UserController {
+    private Map<String, User> users;
+    private User loggedInUser;
+
+    public UserController() {
+        users = new HashMap<>();
+        loggedInUser = null;
+    }
+
+    public void registerUser(User user) {
+        users.put(user.getUsername(), user);
+    }
+
+    public boolean loginUser(String username, String password) {
+        User user = users.get(username);
+        if (user != null && user.getPassword().equals(password)) {
+            loggedInUser = user;
+            return true;
+        }
+        return false;
+    }
+
+    public void logoutUser() {
+        loggedInUser = null;
+    }
+
+    public boolean changePassword(String oldPassword, String newPassword) {
+        if (loggedInUser != null && loggedInUser.getPassword().equals(oldPassword)) {
+            loggedInUser.setPassword(newPassword);
+            return true;
+        }
+        return false;
+    }
+
+    public User getLoggedInUser() {
+        return loggedInUser;
+    }
+}
