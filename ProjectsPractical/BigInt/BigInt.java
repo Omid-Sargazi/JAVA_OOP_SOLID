80336293235cde241ee47cbb7fda74c4c4b8400f
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-06 01:03:59 +0330

    create BigInt class and Constructors

---
+++ ProjectsPractical/BigInt/BigInt.java
@@ -0,0 +1,20 @@
+package ProjectsPractical.BigInt;
+
+import java.math.BigInteger;
+
+public class BigInt {
+
+    private BigInteger value;
+
+    public BigInt() {
+        this.value = BigInteger.ZERO;
+    }
+
+    public BigInt(String val) {
+        this.value = new BigInteger(val);
+    }
+
+    public BigInt(BigInteger val) {
+        this.value = val;
+    }
+}
