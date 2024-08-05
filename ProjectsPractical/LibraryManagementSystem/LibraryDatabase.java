7cf5a834c7cd297b3f69124ab0be638bc67de499
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-06 01:31:51 +0330

    Library Database

---
+++ ProjectsPractical/LibraryManagementSystem/LibraryDatabase.java
@@ -0,0 +1,20 @@
+package ProjectsPractical.LibraryManagementSystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibraryDatabase {
+    private List<Book> books;
+
+    public LibraryDatabase() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+}
