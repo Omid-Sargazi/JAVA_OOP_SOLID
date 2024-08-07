3c721ed8d97d14c9a3e830e665e55bf6e5d05dd6
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-07 18:17:07 +0330

    Interface Segregation Principle (ISP)

---
+++ ProjectsPractical/LibraryManagementSystem/LibraryBookManager.java
@@ -0,0 +1,13 @@
+package ProjectsPractical.LibraryManagementSystem;
+
+public class LibraryBookManager implements BookManager {
+    @Override
+    public void addBook(Book book) {
+        System.out.println("Book added: " + book.getTitle());
+    }
+
+    @Override
+    public void removeBook(Book book) {
+        System.out.println("Book removed: " + book.getTitle());
+    }
+}
