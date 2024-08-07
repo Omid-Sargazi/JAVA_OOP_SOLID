6292c78ea7738dcda392a98155b6c3ad6dfa2bc1
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-07 18:14:19 +0330

    EmailNotification OCP

---
+++ ProjectsPractical/LibraryManagementSystem/EmailNotification.java
@@ -0,0 +1,8 @@
+package ProjectsPractical.LibraryManagementSystem;
+
+public class EmailNotification implements Notification {
+    @Override
+    public void send(String message) {
+        System.out.println("Email Notification: " + message);
+    }
+}
