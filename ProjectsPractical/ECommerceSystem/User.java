d4a51dcb639ad045dc986d4dc4cd5e3c7656830a
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-08 21:51:55 +0330

    Inheritance and Polymorphism

---
+++ ProjectsPractical/ECommerceSystem/User.java
@@ -0,0 +1,26 @@
+package ProjectsPractical.ECommerceSystem;
+
+public abstract class User {
+    private String username;
+    private String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public abstract void purchase(Product product);
+
+    @Override
+    public String toString() {
+        return "User{" + "username='" + username + '\'' + ", email='" + email + '\'' + '}';
+    }
+}
