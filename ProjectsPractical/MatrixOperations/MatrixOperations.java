04cb4d9870a1c5a82a126889eb60d63fc5526ecc
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-10 00:37:45 +0330

    Method to print the nth row of the matrix

--- ProjectsPractical/MatrixOperations/MatrixOperations.java
+++ ProjectsPractical/MatrixOperations/MatrixOperations.java
@@ -19,4 +19,12 @@ public class MatrixOperations {
         }
         System.out.println();
     }
+
+    public static void printNthRow(int[][] matrix, int n) {
+        System.out.println("Row " + n + ":");
+        for (int j = 0; j < matrix[n].length; j++) {
+            System.out.print(matrix[n][j] + " ");
+        }
+        System.out.println();
+    }
 }
