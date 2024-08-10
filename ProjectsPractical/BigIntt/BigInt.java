a79b6a080d54af60ce012dd4b991e0d301659b31
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-10 22:58:42 +0330

    Addition

--- ProjectsPractical/BigIntt/BigInt.java
+++ ProjectsPractical/BigIntt/BigInt.java
@@ -25,4 +25,19 @@ public class BigInt {
         this.digits = digits;
         this.isNegative = isNegative;
     }
+
+    public BigInt add(BigInt other) {
+        if (this.isNegative == other.isNegative) {
+            return new BigInt(addArrays(this.digits, other.digits), this.isNegative);
+        } else {
+            int comparison = compareAbsolute(other);
+            if (comparison == 0) {
+                return new BigInt("0");
+            } else if (comparison > 0) {
+                return new BigInt(subtractArrays(this.digits, other.digits), this.isNegative);
+            } else {
+                return new BigInt(subtractArrays(other.digits, this.digits), other.isNegative);
+            }
+        }
+    }
 }
