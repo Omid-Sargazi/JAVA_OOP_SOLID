d02280bf02d63bd3e0431fa615090ebb21094629
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-12 00:10:34 +0330

    Interface person

---
+++ ProjectsPractical/Interfaces/Person.java
@@ -0,0 +1,16 @@
+package ProjectsPractical.Interfaces;
+
+public class Person implements Comparable<Person> {
+    private String name;
+    private String age;
+
+    public Person(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return Integer.compare(this.age, other.age);
+    }
+}
