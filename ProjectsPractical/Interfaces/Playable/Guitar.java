7f5a6a2cfca05d37badf8e0510c7c9c45ea193d8
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-12 00:14:21 +0330

    Guitar Interface

---
+++ ProjectsPractical/Interfaces/Playable/Guitar.java
@@ -0,0 +1,8 @@
+package ProjectsPractical.Interfaces.Playable;
+
+public class Guitar implements Playable {
+    @Override
+    public void play() {
+        System.out.println("Playing the guitar");
+    }
+}
