72e2edaaeeebfb1b33e2b104f2059cb659ea4d8a
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-12 00:11:45 +0330

    Bird Interface

---
+++ ProjectsPractical/Interfaces/Flyable/Bird.java
@@ -0,0 +1,8 @@
+package ProjectsPractical.Interfaces.Flyable;
+
+public class Bird implements Flyable {
+    @Override
+    public void fly() {
+        System.out.println("Bird is flying");
+    }
+}
