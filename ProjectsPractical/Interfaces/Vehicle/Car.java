bd0c30a877a482d6f385fef9a52743501d8da50d
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-13 00:13:03 +0330

    Car interface

---
+++ ProjectsPractical/Interfaces/Vehicle/Car.java
@@ -0,0 +1,8 @@
+package ProjectsPractical.Interfaces.Vehicle;
+
+public class Car implements Vehicle {
+    @Override
+    public void start() {
+        System.out.println("Car started");
+    }
+}
