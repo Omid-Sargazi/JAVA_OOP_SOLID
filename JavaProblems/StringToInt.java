8ed054a83c2df9b2382a42040fb94bb616815904
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-13 19:20:55 +0330

    String to integer

--- JavaProblems/StringToInt.java
+++ JavaProblems/StringToInt.java
@@ -3,10 +3,18 @@ package JavaProblems;
 public class StringToInt {
     public static void main(String[] args) {
         String str = "omid1235odc";
-        for (int i = 0; i < str.length(); i++) {
+        int num = 0;
+        int count = 1;
+        for (int i = str.length() - 1; i >= 0; i--) {
             if ((str.charAt(i) - '0') >= 0 && str.charAt(i) - '0' <= 9)
-                System.out.println(str.charAt(i));
+
+            {
+                System.out.println(str.charAt(i) - '0');
+                num += (str.charAt(i) - '0') * count;
+                count *= 10;
+            }
         }
+        System.out.println(num + "num");
 
     }
 }
