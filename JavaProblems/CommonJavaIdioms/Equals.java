257a21525bcb0e2191e05fa1e35c5810e785cac6
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-17 00:54:23 +0330

    Using equals() for Object Comparison

---
+++ JavaProblems/CommonJavaIdioms/Equals.java
@@ -0,0 +1,13 @@
+package JavaProblems.CommonJavaIdioms;
+
+public class Equals {
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = "hello";
+        if (a.equals(b)) {
+            System.out.println("a and b are equal...");
+        } else {
+            System.out.println("A and B aren't equal...");
+        }
+    }
+}
