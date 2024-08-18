1bee0f36e0a850667d7a6f6bf35eca120936a063
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-19 00:04:53 +0330

    Book class

---
+++ JavaProblems/LibrarySystem/Book.java
@@ -0,0 +1,13 @@
+package JavaProblems.LibrarySystem;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+
+    public Book(String isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+}
