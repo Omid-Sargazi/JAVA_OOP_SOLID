c30d65d34421460025e5bb51a44fa1ba7da32c57
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-19 00:02:33 +0330

    Patron class inheritance with User

---
+++ JavaProblems/LibrarySystem/Patron.java
@@ -0,0 +1,15 @@
+package JavaProblems.LibrarySystem;
+
+public class Patron extends User {
+    public Patron(String id, String name, String email) {
+        super(id, name, email);
+    }
+
+    @Override
+    public void login() {
+    }
+
+    @Override
+    public void logout() {
+    }
+}
