5f34d057b3eec76172649bec9954dabfbc94864b
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-19 00:06:39 +0330

    Author class

---
+++ JavaProblems/LibrarySystem/Author.java
@@ -0,0 +1,11 @@
+package JavaProblems.LibrarySystem;
+
+public class Author {
+    private String name;
+    private String bio;
+
+    public Author(String name, String bio) {
+        this.name = name;
+        this.bio = bio;
+    }
+}
