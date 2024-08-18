d9a9d38cd00b4b08859b7a206ed6a8917f4b33fe
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-18 22:01:54 +0330

    String Concatenation

--- JavaProblems/StringClass.java
+++ JavaProblems/StringClass.java
@@ -10,8 +10,22 @@ public class StringClass {
         return length;
     }
 
+    public static String concatenation(String str1, String str2) {
+        char[] result = new char[length(str2) + length(str1)];
+        int index = 0;
+
+        for (char c : str1.toCharArray()) {
+            result[index++] = c;
+        }
+        for (char c : str2.toCharArray()) {
+            result[index++] = c;
+        }
+        return new String(result);
+    }
+
     public static void main(String[] args) {
         System.out.println(length("Omid sargazi"));
+        System.out.println(concatenation("Omid", "Sargazi"));
 
     }
 }
